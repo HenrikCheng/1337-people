e52165571b5c847597a390f71a2fdededec4da28
HenrikCheng <48348033+HenrikCheng@users.noreply.github.com>
2022-08-12 07:42:27 +0200

    added icons & responsiveness

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import "./App.css";
 import axios from "axios";
 import useSWR from "swr";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faGithubSquare,
+  faTwitterSquare,
+} from "@fortawesome/free-brands-svg-icons";
+
+import "./App.css";
 
 function App() {
-  const fetcher = (url, token) =>
+  const fetcher = (url) =>
     axios
       .get(url, {
         headers: {
@@ -21,22 +29,40 @@ function App() {
 
   console.log(data);
   return (
-    <div className="flex flex-row flex-wrap">
-      {data.map((person) => (
-        <div key={person.email} className="w-1/4 p-2 flex flex-col">
-          {person.imagePortraitUrl ? (
-            <img src={person.imagePortraitUrl} alt="" />
-          ) : (
-            <img
-              className="w-full h-full object-cover"
-              src="https://i0.wp.com/tungelfastigheter.se/wp-content/uploads/2018/04/default-user-img.jpg?ssl=1"
-              alt=""
-            />
-          )}
-          <h3>{person.name}</h3>
-          <p>Office: {person.office}</p>
-        </div>
-      ))}
+    <div className="bg-gray-100 sm:p-4">
+      <h1 className="text-2xl bold">The fellowship of the tretton37</h1>
+      <div className="flex flex-row flex-wrap">
+        {data.map((person) => (
+          <div
+            key={`${person.phoneNumber}_${person.email}`}
+            className="w-1/2 xl:w-1/6 lg:w-1/5 md:w-1/4 sm:w-1/3 p-2 flex flex-col justify-end"
+          >
+            {person.imagePortraitUrl ? (
+              <img
+                src={person.imagePortraitUrl}
+                alt=""
+                className="h-full w-full object-cover"
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faUser}
+                className="w-full h-full pt-16 bg-white"
+              />
+            )}
+            <div className="flex flex-col sm:flex-row justify-between p-4">
+              <div>
+                <h3>{person.name}</h3>
+                <p>Office: {person.office}</p>
+              </div>
+              <div className="space-x-2 py-2 flex flex-nowrap">
+                <FontAwesomeIcon icon={faLinkedin} className="fa-xl" />
+                <FontAwesomeIcon icon={faGithubSquare} className="fa-xl" />
+                <FontAwesomeIcon icon={faTwitterSquare} className="fa-xl" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
