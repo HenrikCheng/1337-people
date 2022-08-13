0057d7508c46c8166eb1c370ac34fae2324dff35
HenrikCheng <48348033+HenrikCheng@users.noreply.github.com>
2022-08-13 11:42:28 +0200

    add fancy loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+
+const Loader = () => (
+  <div
+    role="status"
+    className="w-full h-screen flex items-start justify-center pt-20"
+  >
+    <FontAwesomeIcon icon={faGear} className="w-40 h-40 animate-spin" />
+    <FontAwesomeIcon icon={faGear} className="w-40 h-40 animate-reverse-spin mt-10 text-gray-500" />
+    <span class="sr-only">Loading...</span>
+  </div>
+);
+
+export default Loader;
